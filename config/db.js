358041a4d7cbc3980d6f659b6a583bdeba5fcd15
emilioQuilodran <emilio.lihue.q@gmail.com>
2022-04-27 11:45:33 -0300

    fix db conexion

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const { dbUsername, dbPassword, dbHost, dbName } = require('.');
 
 const connection = async () => {
     const url = `mongodb+srv://${dbUsername}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`;
+    
     const conn = await mongoose.connect(url)
     console.log("mongo db connected:" ,conn.connection.host);
 }
